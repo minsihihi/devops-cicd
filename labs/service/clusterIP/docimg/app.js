6a2ee4ce42f964bec3355b26dbf1d0794d6e2b27
minsihihi <qkralstj0926@gmail.com>
2025-07-15 12:03:21 +0900

    app init setup (nodeJS/Dockerfile)

---
+++ labs/service/clusterIP/docimg/app.js
@@ -0,0 +1,15 @@
+// nodes app img
+// os/http 패키징해서 request 들어오면 hostname 출력 (8080 listen)
+const http = require('http');
+const os = require('os');
+
+console.log("Kubia server starting...");
+
+var handler = function(request, response) {
+  console.log("Received request from " + request.connection.remoteAddress);
+  response.writeHead(200);
+  response.end("You've hit " + os.hostname() + "\n");
+};
+
+var www = http.createServer(handler);
+www.listen(8080);
